3f5a002d84504ff0148e375ac3c9900ca23e7af6
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-20 07:40:04 -0500

    setup comments module

---
+++ App/Modules/Comments/CommentsScreen.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react'
+import { View, ScrollView, Text } from 'react-native'
+
+class CommentsScreen extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit() {
+
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View>
+
+        </View>
+        <View>
+
+        </View>
+      </ScrollView>
+    )
+  }
+}
+
+export default CommentsScreen
---
+++ App/Modules/Comments/CommentsScreenStyles.js
@@ -0,0 +1,3 @@
+import { StylesSheet } from 'react-native';
+
+

bb0780e7899f5535dc49c1b040bbbda4285152b0
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-10-01 10:33:00 -0500

    added apollo provider

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,25 +1,15 @@
 import '../config';
-import React, { Component } from 'react';
+import React from 'react';
+import { ApolloClient, ApolloProvider } from 'react-apollo';
+
 import RootContainer from './RootContainer';
 
+const client = new ApolloClient();
 
-/**
- * Provides an entry point into our application.  Both index.ios.js and index.android.js
- * call this component first.
- *
- * We create our Redux store here, put it into a provider and then bring in our
- * RootContainer.
- *
- * We separate like this to play nice with React Native's hot reloading.
- */
+const App = () => (
+  <ApolloProvider client={client}>
+    <RootContainer />
+  </ApolloProvider>
+);
 
-class App extends Component {
-  render() {
-    return (
-      <RootContainer />
-    );
-  }
-}
-
-// allow reactotron overlay for fast design in dev mode
 export default App;

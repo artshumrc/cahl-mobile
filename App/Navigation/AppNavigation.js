1061db829eb7403dcf1b7c2882e340b729c45193
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-10-02 18:33:58 -0500

    finally fixed the header for good. the journey was long and hard, but after fight endless bugs without any stack trace and reading pretty much every git issue ever, we got there.

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,52 +1,18 @@
 import React from 'react';
-import { StackNavigator, TabNavigator } from 'react-navigation';
-import Flag from 'react-native-flags';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { StackNavigator } from 'react-navigation';
+
+import CustomTabs from './CustomTabs';
 
 // views
 import LoginScreen from '../modules/Login';
 import CommentsScreen from '../modules/Comments';
-import ExhibitsScreen from '../modules/Exhibits';
-import StoriesScreen from '../modules/Stories';
-import SearchScreen from '../modules/Search';
 
 // styles
 import styles from './NavigationStyles';
 
-// TODO: Position header correctly for tab navigator
-
-
-const ExhibitStoriesTabNav = TabNavigator({
-  ExhibitsTab: {
-    screen: ExhibitsScreen,
-  },
-  StoriesTab: {
-    screen: StoriesScreen,
-  },
-  SearchTab: {
-    screen: SearchScreen,
-    navigationOptions: {
-      tabBarLabel: <Icon name="search" style={styles.searchLabel} />,
-    },
-  },
-  languageTab: {
-    screen: ExhibitsScreen,
-    navigationOptions: {
-      tabBarLabel: <Flag code="FR" size={24} type="shiny" style={styles.languageLabel} />,
-    },
-  },
-}, {
-  tabBarPosition: 'top',
-  tabBarOptions: {
-    style: styles.tabHeader,
-    labelStyle: styles.tabLabel,
-    activeTintColor: '#000',
-  },
-});
-
 const PrimaryNav = StackNavigator({
   Root: {
-    screen: ExhibitStoriesTabNav,
+    screen: CustomTabs,
     navigationOptions: {
       header: null,
     },

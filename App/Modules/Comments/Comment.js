7a5e2cb5c4315a07c6617c1d9f991e310eca851c
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-05 08:38:08 -0600

    added moment js to format date

--- App/Modules/Comments/Comment.js
+++ App/Modules/Comments/Comment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, Image } from 'react-native';
+import moment from 'moment';
 
 // styles
 import styles from './CommentStyles';
@@ -15,7 +16,7 @@ const Comment = ({ content, displayName, createdAt, photoURL }) => (
           <Text style={styles.text}>{content}</Text>
         </Text>
       </View>
-      <Text style={styles.date}>{createdAt}</Text>
+      <Text style={styles.date}>{moment(Date(createdAt)).format("D MMMM YYYY")}</Text>
     </View>
   </View>
 );

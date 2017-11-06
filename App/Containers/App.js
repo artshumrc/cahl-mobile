b47e20ebf7fb6f02967040ce8c39ed204f5bf8d2
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-06 15:51:40 -0600

    pass token to header

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -3,12 +3,24 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import firebase from 'firebase';
 
 import RootContainer from './RootContainer';
 
 const networkInterface = createNetworkInterface({
   uri: 'http://api.cahl.orphe.us/graphql',
 });
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}; // Create the header object if needed.
+    }
+    req.options.headers.authorization = firebase.auth().currentUser ? firebase.auth().currentUser.getIdToken() : null;
+    next();
+  }
+}]);
+
 const client = new ApolloClient({
   networkInterface,
 });

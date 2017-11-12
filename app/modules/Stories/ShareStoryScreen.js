f2d6d5dc52a1279ec89a5276d89a01597863aaf9
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-12 10:52:50 -0600

    added aws s3 uploader and method

--- app/modules/Stories/ShareStoryScreen.js
+++ app/modules/Stories/ShareStoryScreen.js
@@ -20,6 +20,11 @@ I18n.translations = {
   fr,
 };
 
+const AWS_ACCESS_KEY_ID='AKIAJLHEKA2FQXGV6YVA';
+const AWS_SECRET_ACCESS_KEY='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';
+const AWS_BUCKET='iiif-orpheus';
+const AWS_REGION='us-east-1';
+
 class ShareStory extends React.Component {
   constructor(props) {
     super(props);
@@ -63,6 +68,39 @@ class ShareStory extends React.Component {
         });
       }
     });
+
+    const AWSoptions = {
+      keyPrefix: "uploads/",
+      bucket: AWS_BUCKET,
+      region: AWS_REGION,
+      accessKey: AWS_ACCESS_KEY_ID,
+      secretKey: AWS_SECRET_ACCESS_KEY,
+      successActionStatus: 201,
+    };
+
+    const file = {
+      uri: this.state.photoURL,
+      type: "image/png",
+      name: "image.png",
+    }
+
+    RNS3.put(file, AWSoptions).then(response => {
+      if (response.status !== 201)
+        throw new Error("Failed to upload image to S3");
+      console.log(response.body);
+      /**
+       * {
+       *   postResponse: {
+       *     bucket: "your-bucket",
+       *     etag : "9f620878e06d28774406017480a59fd4",
+       *     key: "uploads/image.png",
+       *     location: "https://your-bucket.s3.amazonaws.com/uploads%2Fimage.png"
+       *   }
+       * }
+       */
+    });
+
+
   }
 
   render() {
